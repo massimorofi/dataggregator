081233737ddd17279a08718906e223ac82c251b2
rofi <massimo.rofi@gmail.com>
2020-04-25 22:17:03 +0200

    added aggregation

--- src/main/java/DataUpdater.java
+++ src/main/java/DataUpdater.java
@@ -1,29 +1,12 @@
 
-import java.net.URL;
-import java.io.*;
-import javax.net.ssl.HttpsURLConnection;
-
 public class DataUpdater {
 
 	public static void main(String[] args) {
-		try {
-			String httpsURL = "https://news.yahoo.com/rss/latest";
-			URL myUrl = new URL(httpsURL);
-			HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
-			InputStream is = conn.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			String inputLine;
-			BufferedWriter f = new BufferedWriter(new FileWriter("dist/data/RSS_News.xml"));
-			while ((inputLine = br.readLine()) != null) {
-				System.out.println(inputLine);
-				f.write(inputLine);
-			}
-			br.close();
-			f.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("Aggregating RSS FEEDS ...");
+		RSSAggregator.run();
+		System.out.println("Aggregating COVID CSV DATA ...");
+		COVID_Aggregate.run();
+        System.out.println("DATAUPDATER-->END.");
 	}
 
 }
