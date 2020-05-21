30b754629fbba6c6e5088252af799c0b662db546
rofi <massimo.rofi@gmail.com>
2020-05-21 18:32:35 +0200

    fixed table change error

--- src/main/java/HTMLRemoteLoader.java
+++ src/main/java/HTMLRemoteLoader.java
@@ -36,7 +36,7 @@ public class HTMLRemoteLoader {
 			htmlToCsv();
 			// Aggregate data and save
 			aggregate();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -45,20 +45,21 @@ public class HTMLRemoteLoader {
 	public static void aggregate() throws SQLException {
 		Connection dbCon = DriverManager.getConnection("jdbc:h2:mem:");
 		Statement stm = dbCon.createStatement();
-		stm.executeUpdate("CREATE TABLE covid(\r\n" + "   Country           VARCHAR(30)\r\n"
+		stm.executeUpdate("CREATE TABLE covid(\r\n" + "   ORD INTEGER\r\n" + "  ,Country           VARCHAR(30)\r\n"
 				+ "  ,Total_Cases       INTEGER \r\n" + "  ,New_Cases         INTEGER \r\n"
 				+ "  ,Total_Deaths      INTEGER \r\n" + "  ,New_Deaths        INTEGER \r\n"
 				+ "  ,Total_Recovered   INTEGER \r\n" + "  ,Active_Cases      INTEGER \r\n"
 				+ "  ,Serious_Critical  INTEGER \r\n" + "  ,Tot_Cases_1M_pop  VARCHAR(30) \r\n"
 				+ "  ,Deaths_1M_pop     VARCHAR(30) \r\n" + "  ,Total_Tests       INTEGER \r\n"
-				+ "  ,Tests_1M_pop      INTEGER \r\n" + "  ,Continent         VARCHAR(30) \r\n" + ");");
+				+ "  ,Tests_1M_pop      INTEGER \r\n" + "  ,Population          INTEGER\r\n"
+				+ "  ,Continent         VARCHAR(30) \r\n" + ");");
 		stm.executeUpdate(
-				"INSERT INTO \"PUBLIC\".\"COVID\" (COUNTRY,TOTAL_CASES,NEW_CASES,TOTAL_DEATHS,NEW_DEATHS,TOTAL_RECOVERED,ACTIVE_CASES,SERIOUS_CRITICAL,TOT_CASES_1M_POP,DEATHS_1M_POP,TOTAL_TESTS,TESTS_1M_POP,CONTINENT) \r\n"
-						+ "SELECT COUNTRY,TOTAL_CASES,NEW_CASES,TOTAL_DEATHS,NEW_DEATHS,TOTAL_RECOVERED,ACTIVE_CASES,SERIOUS_CRITICAL,TOT_CASES_1M_POP,DEATHS_1M_POP,TOTAL_TESTS,TESTS_1M_POP,CONTINENT \r\n"
-						+ "FROM CSVREAD('tmp/covid_world_today.csv','COUNTRY;TOTAL_CASES;NEW_CASES;TOTAL_DEATHS;NEW_DEATHS;TOTAL_RECOVERED;ACTIVE_CASES;SERIOUS_CRITICAL;TOT_CASES_1M_POP;DEATHS_1M_POP;TOTAL_TESTS;TESTS_1M_POP;CONTINENT','fieldSeparator=;');");
+				"INSERT INTO \"PUBLIC\".\"COVID\" (ORD,COUNTRY,TOTAL_CASES,NEW_CASES,TOTAL_DEATHS,NEW_DEATHS,TOTAL_RECOVERED,ACTIVE_CASES,SERIOUS_CRITICAL,TOT_CASES_1M_POP,DEATHS_1M_POP,TOTAL_TESTS,TESTS_1M_POP,POPULATION,CONTINENT) \r\n"
+						+ "SELECT ORD,COUNTRY,TOTAL_CASES,NEW_CASES,TOTAL_DEATHS,NEW_DEATHS,TOTAL_RECOVERED,ACTIVE_CASES,SERIOUS_CRITICAL,TOT_CASES_1M_POP,DEATHS_1M_POP,TOTAL_TESTS,TESTS_1M_POP,POPULATION,CONTINENT \r\n"
+						+ "FROM CSVREAD('tmp/covid_world_today.csv','ORD;COUNTRY;TOTAL_CASES;NEW_CASES;TOTAL_DEATHS;NEW_DEATHS;TOTAL_RECOVERED;ACTIVE_CASES;SERIOUS_CRITICAL;TOT_CASES_1M_POP;DEATHS_1M_POP;TOTAL_TESTS;TESTS_1M_POP;POPULATION;CONTINENT','fieldSeparator=;');");
 		// AGGREGATE DATA
 		ResultSet rs = stm.executeQuery("SELECT *, CURRENT_DATE() AS DATE FROM COVID \r\n"
-				+ "WHERE COUNTRY IN ('Italy','Germany','Spain','France', 'UK','USA','Switzerland','Austria','Japan','S. Korea') \r\n"
+				+ "WHERE COUNTRY IN ('Italy','Germany','Spain','France', 'UK','USA','Russia','Sweden','Japan','Brazil') \r\n"
 				+ "ORDER BY TESTS_1M_POP DESC;");
 		(new Csv()).write("dist/data/covid-stats.csv", rs, null);
 	}
@@ -73,16 +74,18 @@ public class HTMLRemoteLoader {
 			// Tables 0=today, 1=sumary today; 2= total; 3= yesterday
 			Element table = doc.select("tbody").get(0);
 			Elements rows = table.select("tr");
-			//Elements ths = doc.select("th");
+			Elements ths = doc.select("th");
+
 			/*
 			 * String thstr = ""; int eol = ths.size() - 1; System.out.println(eol); int
 			 * counter = 0; for (Element th : ths) { if (counter < eol) { thstr += th.text()
 			 * + ";"; } else { thstr += th.text() + "\n"; } counter++; } thstr =
-			 * thstr.trim().replaceAll(",", "_").replaceAll("/", "_").replaceAll("
-			 * ", "_")+"\n";
+			 * thstr.trim().replaceAll(",", "_").replaceAll("/", "_").replaceAll("", "_") +
+			 * "\n";
+			 * 
+			 * f.write(thstr.getBytes());
 			 */
-			// f.write(thstr.getBytes());
-			// System.out.println("-->" + doc.select("tbody").size());
+			System.out.println("-->" + doc.select("tbody").size());
 			for (Element row : rows) {
 				Elements tds = row.select("td");
 				String line = "";
@@ -136,7 +139,8 @@ public class HTMLRemoteLoader {
 	 */
 	public static Document getHtmlDoc() throws MalformedURLException, IOException, ProtocolException {
 		URL url = new URL("https://www.worldometers.info/coronavirus/");
-		//Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy-here", 8080));
+		// Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy-here",
+		// 8080));
 		HttpURLConnection uc = (HttpURLConnection) url.openConnection();
 		uc.setRequestProperty("User-Agent", "Mozilla/5.0");
 		uc.setRequestProperty("Content-Language", "en-US");
